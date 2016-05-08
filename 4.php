683c768fc719d76b196b91e45089a15aa063141c
pavan <pavan0654@gmail.com>
2016-05-08 23:37:32 +0530

    Create 4.php

---
+++ 4.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-color:'blue';
+	background-repeat:no-repeat;
+	background-size: 1400px 600px;
+}
+</style>
+</head>
+<body>
+<form action="login.php" method="get">
+<center>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+  LOGIN-ID:
+  <input id="firstname" type="text" name="firstname">
+  <br>
+  <br>
+  PASSWORD:
+  <input id="password" type="password" name="password">
+  <br>
+  <br>
+ <input type="submit" value="submit"/>
+</form>
+
+</body>
+</html>
